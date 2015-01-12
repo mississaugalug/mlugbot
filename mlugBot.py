1e1c4a8d9102681373e78f54b38e15f1df4c93e7
Victor Mendonca <victorbrca@yahoo.ca>
2015-01-11 23:20:36 -0500

    Added '!meet'

--- mlugBot.py
+++ mlugBot.py
@@ -245,17 +245,31 @@ class LogBot(irc.IRCClient):
 
     # Meet
         elif msg == "!meet":
-            msg = "I'm not programmed with this option yet"
+            url = 'http://mississaugalug.ca/'
+            soup = BeautifulSoup(urllib2.urlopen("%s" % url))
+
+            getEventTitles = soup.findAll(itemprop="name")
+
+            cnt = 0
+            for title in getEventTitles:
+                thisTitle = title.getText()
+                if thisTitle == "MLUG Monthly Meet":
+                    positionalP = cnt
+                    break
+                cnt += 1
+
+            title = soup.findAll(itemprop="name")[positionalP].getText()
+            month = soup.findAll("div", {"class": "dp-upcoming-text-month"})[positionalP].getText()
+            day = soup.findAll("div", {"class": "dp-upcoming-text-day"})[positionalP].getText()
+            startDate = soup.findAll(itemprop="startDate")[positionalP].getText()
+            time = startDate.split('-', 1)[0].split(' ', 1)[1]
+            location = soup.findAll("meta", {"itemprop": "location"})[positionalP]['content']
+            spltLoc = location.rsplit(',', 2)[0]
+
+            meet = 'The next \"%s\" will be on %s %s, %sat %s' % (title, month, day, time, spltLoc)
+            msg = meet.encode('utf-8')
             self.msg(channel, msg)
             self.logger.log("<%s> %s" % (self.nickname, msg))
-#            f=open("lib/fun")
-#            fun = f.read()
-#            for line in fun.split(os.linesep):
-#                msg = ("%s" % line)
-#                self.msg(channel, msg)
-#                self.logger.log("<%s> %s" % (self.nickname, msg))
-#                #time.sleep(.8)
-#            f.close()
 
     # Wikipedia
         elif re.search(r"!wiki .*", msg):
